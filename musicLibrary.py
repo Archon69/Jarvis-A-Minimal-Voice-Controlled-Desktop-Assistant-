445050fd7ea5facdad6b53b9da4a354bf50e5e21
Archon69 <akameet2004@gmail.com>
2025-08-28 19:34:57 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "aura": "https://www.youtube.com/watch?v=8jhC9xnJGYU&t=48s",
+    "skyfall":"https://www.youtube.com/watch?v=XmDnj_Hrxf8&list=RDXmDnj_Hrxf8&start_radio=1",
+    "kill": "https://www.youtube.com/watch?v=hWYGb5TdYeg&list=RDEMKNlX2peDRQpNEucSbh2qMA&start_radio=1&rv=BsgmziA4oi4",
+    "old":"https://www.youtube.com/watch?v=7UGOIMoJtB4&list=RD7UGOIMoJtB4&start_radio=1"
+
+}
